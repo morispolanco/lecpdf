42a49cda4072fe7f621ad45fc53c5028ef9488b2
morispolanco <morispolanco@gmail.com>
2023-06-26 12:26:48 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,61 +3,55 @@ import pdfplumber
 import openai
 import os
 
-# Load GPT-3 API key from secrets
+# Cargar la clave de la API de GPT-3 desde las variables de entorno
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-# Function to read and extract text from PDF
-def read_pdf(file):
-    with pdfplumber.open(file) as pdf:
-        text = ""
-        for page in pdf.pages:
-            text += page.extract_text()
-    return text
+# Función para leer y extraer texto de un archivo PDF
+def leer_pdf(archivo):
+    with pdfplumber.open(archivo) as pdf:
+        texto = ""
+        for pagina in pdf.pages:
+            texto += pagina.extract_text()
+    return texto
 
-# Function to split text into chunks
-def split_text(text, chunk_size):
-    chunks = []
-    for i in range(0, len(text), chunk_size):
-        chunks.append(text[i:i+chunk_size])
-    return chunks
+# Función para dividir el texto en fragmentos
+def dividir_texto(texto, tamaño_fragmento):
+    fragmentos = []
+    for i in range(0, len(texto), tamaño_fragmento):
+        fragmentos.append(texto[i:i+tamaño_fragmento])
+    return fragmentos
 
-# Main app
+# Aplicación principal
 def main():
-    st.title("PDF Reader and Question Answering App")
-    
-    # File upload
-    uploaded_file = st.file_uploader("Upload PDF", type="pdf")
-    
-    if uploaded_file is not None:
-        # Read PDF and extract text
-        text = read_pdf(uploaded_file)
-        
-        # Split text into chunks
-        chunk_size = 1000
-        chunks = split_text(text, chunk_size)
-        
-        # Question input
-        question = st.text_input("Enter your question")
-        
-        if st.button("Ask"):
-            # Use GPT-3 to answer question
-            answer = openai.Completion.create(
+    st.title("Aplicación de Lectura de PDF y Preguntas y Respuestas")
+
+    # Subir archivo
+    archivo_subido = st.file_uploader("Subir PDF", type="pdf")
+
+    if archivo_subido is not None:
+        # Leer el PDF y extraer el texto
+        texto = leer_pdf(archivo_subido)
+
+        # Dividir el texto en fragmentos
+        tamaño_fragmento = 1000
+        fragmentos = dividir_texto(texto, tamaño_fragmento)
+
+        # Entrada de la pregunta
+        pregunta = st.text_input("Ingrese su pregunta")
+
+        if st.button("Preguntar"):
+            # Utilizar GPT-3 para responder la pregunta
+            respuesta = openai.Completion.create(
                 engine="davinci",
-                prompt=question,
+                prompt=pregunta,
                 max_tokens=100,
                 temperature=0.7,
                 n=1,
                 stop=None,
                 log_level="info"
             )
-            
-            st.write("Answer:", answer.choices[0].text)
-    
+
+            st.write("Respuesta:", respuesta.choices[0].text)
+
 if __name__ == "__main__":
     main()
-
-
-
-
-
-
